9b1db31ce9f54bfb739bd7f1a9b7ce71dddce50e
Ashin Shabi <ashinshabi88@gmail.com>
2025-07-24 16:07:44 +0000

    nah bro again this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
